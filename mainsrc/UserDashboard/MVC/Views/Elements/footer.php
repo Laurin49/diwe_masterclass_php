e24b71bb13248c47bccb788ca46d9c43a4bf1a1b
Laurin49 <welter@diwe.info>
2022-12-12 12:06:07 +0100

    Chapter 26 User Dashboard Navbar o.k.

---
+++ mainsrc/UserDashboard/MVC/Views/Elements/footer.php
@@ -0,0 +1,32 @@
+<div class="container-fluid p-3 mb-2 bg-light text-dark">
+    <br>
+    <br>
+    <div class="container">
+        <br>
+        <div class="row align-items-start">
+            <div class="col">
+                <h5>Zu uns</h5>
+                <hr>
+                <a href="#" class="nav-link">Kontakt</a>
+                <a href="#" class="nav-link">Versand</a>
+                <a href="#" class="nav-link">Rechtliches</a>
+            </div>
+            <div class="col">
+                <h5>Community</h5>
+                <hr>
+                <a href="#" class="nav-link">Forum</a>
+                <a href="#" class="nav-link">Support</a>
+                <a href="#" class="nav-link">Nachrichten</a>
+            </div>
+            <div class="col">
+                <h5>Account</h5>
+                <hr>
+                <a href="#" class="nav-link">Settings</a>
+                <a href="#" class="nav-link">Profil</a>
+                <a href="#" class="nav-link">Logout</a>
+            </div>
+        </div>
+    </div>
+    <br>
+    <br>
+</div>
---
+++ mainsrc/UserDashboard/MVC/Views/Elements/navbar.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/masterclass">
+            <img src="https://getbootstrap.com/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top">
+            Masterclass
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/masterclass">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/Settings">Settings</a>
+                </li>
+            </ul>
+            <a class="float-end" href="Logout"><button class="btn btn-secondary">Logout</button></a>
+        </div>
+    </div>
+</nav>
